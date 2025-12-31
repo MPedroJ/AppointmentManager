be088b2420022e363ec2d68e6ba2faa91fa3850b
Pedro Jorge Morales <pedrojorgemorales6@gmail.com>
2025-12-30 23:22:33 -0300

    Fix background gradient to fill entire screen

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,28 +1,29 @@
-import Home from './views/Home/Home'
-import Header from './components/Header/Header'
-import MisTurnos from './views/MyAppointments/MyAppointments'
-import Register from './views/Register/Register'
-import Login from './views/Login/Login'
-import Famous from './views/Famous/Famous'
-import About from './views/About/About'
-import { Routes, Route } from "react-router-dom"
-import ScheduleAppointment from './views/ScheduleAppointment/ScheduleAppointment'
+import Home from './views/Home/Home';
+import Header from './components/Header/Header';
+import MisTurnos from './views/MyAppointments/MyAppointments';
+import Register from './views/Register/Register';
+import Login from './views/Login/Login';
+import Famous from './views/Famous/Famous';
+import About from './views/About/About';
+import { Routes, Route } from 'react-router-dom';
+import ScheduleAppointment from './views/ScheduleAppointment/ScheduleAppointment';
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <div style={{ minHeight: '100vh', width: '100%' }}>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/appointments' element={<MisTurnos />} />
-        <Route path='/famous' element={<Famous />} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/users/register' element={<Register />} />
-        <Route path='/users/login' element={<Login />} />
-        <Route path='/appointments/schedule' element={<ScheduleAppointment />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/appointments" element={<MisTurnos />} />
+        <Route path="/famous" element={<Famous />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users/register" element={<Register />} />
+        <Route path="/users/login" element={<Login />} />
+        <Route path="/appointments/schedule" element={<ScheduleAppointment />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
